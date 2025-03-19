c4499409a6d5b9d481f844e34a3407a7f839ad14
Ruben Pla Ferrero <rubenpla.develop@gmail.com>
2025-03-19 15:49:36 +0100

    chore: update DI Modules for updated data layers

--- app/src/main/java/com/rpla/marvelherosrepo/core/di/AppModule.kt
+++ app/src/main/java/com/rpla/marvelherosrepo/core/di/AppModule.kt
@@ -1,6 +1,11 @@
 package com.rpla.marvelherosrepo.core.di
 
 import android.content.Context
+import com.rpla.marvelherosrepo.data.DataSource
+import com.rpla.marvelherosrepo.data.DataSourceImpl
+import com.rpla.marvelherosrepo.remote.rest.RestApi
+import com.rpla.marvelherosrepo.remote.rest.RestApiImpl
+import com.rpla.marvelherosrepo.remote.retrofit.RetrofitApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -13,4 +18,10 @@ object AppModule {
 
         @Provides
         fun providesContext(@ApplicationContext context: Context) = context
+
+        @Provides
+        fun providesRestApi(retrofitApi: RetrofitApi): RestApi = RestApiImpl(retrofitApi = retrofitApi)
+
+        @Provides
+        fun providesDataSource(restApi: RestApi): DataSource = DataSourceImpl(restApi = restApi)
 }
