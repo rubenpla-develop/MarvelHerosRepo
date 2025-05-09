042c9e59947e6c791aa472401cfb58f872d5fd4c
Ruben Pla Ferrero <rubenpla.develop@gmail.com>
2025-05-09 21:07:42 +0200

    refactor: fix Detekt max length line error

--- app/src/main/java/com/rpla/marvelherosrepo/profile/ui/ProfileScreen.kt
+++ app/src/main/java/com/rpla/marvelherosrepo/profile/ui/ProfileScreen.kt
@@ -209,6 +209,23 @@ fun CharacterDetailPreview() {
 }
 
 const val MEDIUM_LOREM_IPSUM =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Volutpat odio facilisis mauris sit. Amet venenatis urna cursus eget nunc scelerisque viverra mauris. Massa tempor nec feugiat nisl. Convallis a cras semper auctor neque. Lectus sit amet est placerat in. Adipiscing tristique risus nec feugiat."
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt " +
+            "ut labore et dolore magna aliqua. Volutpat odio facilisis mauris sit. Amet venenatis " +
+            "urna cursus eget nunc scelerisque viverra mauris. Massa tempor nec feugiat nisl. " +
+            "Convallis a cras semper auctor neque. Lectus sit amet est placerat in. Adipiscing " +
+            "tristique risus nec feugiat."
 const val HUGE_LOREM_IPSUM =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet cursus sit amet dictum sit amet justo donec enim. Consectetur libero id faucibus nisl tincidunt eget nullam non. Lorem donec massa sapien faucibus et molestie ac feugiat sed. Tellus in metus vulputate eu. Lacus sed viverra tellus in hac habitasse platea dictumst. Duis ut diam quam nulla. Rhoncus urna neque viverra justo nec. Eget nunc lobortis mattis aliquam faucibus purus in. Nunc id cursus metus aliquam eleifend mi in nulla. Blandit turpis cursus in hac habitasse platea dictumst. Enim eu turpis egestas pretium aenean pharetra. Velit egestas dui id ornare arcu odio. Vitae congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque. Velit laoreet id donec ultrices tincidunt arcu. Tellus integer feugiat scelerisque varius morbi enim nunc. Sit amet justo donec enim. Lectus quam id leo in vitae turpis massa sed elementum. Molestie a iaculis at erat pellentesque adipiscing commodo. Felis eget velit aliquet sagittis id."
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt" +
+            " ut labore et dolore magna aliqua. Amet cursus sit amet dictum sit amet justo donec" +
+            " enim. Consectetur libero id faucibus nisl tincidunt eget nullam non. Lorem donec " +
+            "massa sapien faucibus et molestie ac feugiat sed. Tellus in metus vulputate eu. " +
+            "Lacus sed viverra tellus in hac habitasse platea dictumst. Duis ut diam quam nulla. " +
+            "" +
+            "Rhoncus urna neque viverra justo nec. Eget nunc lobortis mattis aliquam faucibus " +
+            "purus in. Nunc id cursus metus aliquam eleifend mi in nulla. Blandit turpis cursus " +
+            "in hac habitasse platea dictumst. Enim eu turpis egestas pretium aenean pharetra. " +
+            "Velit egestas dui id ornare arcu odio. Vitae congue mauris rhoncus aenean vel elit " +
+            "scelerisque mauris pellentesque. Velit laoreet id donec ultrices tincidunt arcu. " +
+            "Tellus integer feugiat scelerisque varius morbi enim nunc. Sit amet justo donec " +
+            "enim. Lectus quam id leo in vitae turpis massa sed elementum. Molestie a iaculis at" +
+            " erat pellentesque adipiscing commodo. Felis eget velit aliquet sagittis id."
--- app/src/main/java/com/rpla/marvelherosrepo/remote/rest/RestApiImpl.kt
+++ app/src/main/java/com/rpla/marvelherosrepo/remote/rest/RestApiImpl.kt
@@ -9,17 +9,22 @@ import com.rpla.marvelherosrepo.profile.remote.response.CharacterDetailResponse
 import com.rpla.marvelherosrepo.remote.retrofit.RetrofitApi
 import javax.inject.Inject
 
-class RestApiImpl @Inject constructor(private val retrofitApi: RetrofitApi): RestApi {
+class RestApiImpl @Inject constructor(private val retrofitApi: RetrofitApi) : RestApi {
 
-    override suspend fun getCharacters(getAllCharactersRequest: GetAllCharactersRequest): CharacterListResponse {
+    override suspend fun getCharacters(getAllCharactersRequest: GetAllCharactersRequest)
+            : CharacterListResponse {
         return retrofitApi.getCharactersList(offset = getAllCharactersRequest.nextPage)
     }
 
-    override suspend fun getCharacterDetail(getCharacterDetailRequest: GetCharacterDetailRequest): CharacterDetailResponse {
+    override suspend fun getCharacterDetail(getCharacterDetailRequest: GetCharacterDetailRequest)
+            : CharacterDetailResponse {
         return retrofitApi.getCharacterDetail(getCharacterDetailRequest.id)
     }
 
-    override suspend fun getCharacterComicsList(getCharacterComicsListRequest: GetCharacterComicsListRequest): CharacterComicsListResponse {
+    override suspend fun getCharacterComicsList(
+        getCharacterComicsListRequest
+        : GetCharacterComicsListRequest
+    ): CharacterComicsListResponse {
         return retrofitApi.getCharacterComicsList(getCharacterComicsListRequest.id)
     }
 }
