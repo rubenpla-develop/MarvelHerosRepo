7d7c8704113f95cc1ba49912ba67ef9eb456fd37
Ruben Pla Ferrero <rubenpla.develop@gmail.com>
2025-05-09 20:53:01 +0200

    refactor: Delete unnecessary comments

--- app/src/main/java/com/rpla/marvelherosrepo/remote/ApiConstants.kt
+++ app/src/main/java/com/rpla/marvelherosrepo/remote/ApiConstants.kt
@@ -8,9 +8,8 @@ object ApiConstants {
     const val PAGE_SIZE = 20
 
     const val STATUS_CODE_500_INTERNAL_ERROR = 500
-    const val STATUS_CODE_409_MISSING_QUERY_PARAM = 409 // Missing API/Hash/Timestamp query param
-    const val STATUS_CODE_401_INVALID_REFERRER_OR_HASH = 401 // Invalid Referrer
-    // Invalid Hash: Occurs when a ts, hash, and apikey parameter are sent but the hash is not valid per the above hash generation rule..
-    const val STATUS_CODE_405_METHOD_NOT_ALLOWED = 405 // Method Not Allowed: Occurs when an API endpoint is accessed using an HTTP verb which is not allowed for that endpoint.
-    const val STATUS_CODE_403_FORBIDDEN = 403 // Forbidden: Occurs when a user with an otherwise authenticated request attempts to access an endpoint which they do not have access to.
+    const val STATUS_CODE_409_MISSING_QUERY_PARAM = 409
+    const val STATUS_CODE_401_INVALID_REFERRER_OR_HASH = 401
+    const val STATUS_CODE_405_METHOD_NOT_ALLOWED = 405
+    const val STATUS_CODE_403_FORBIDDEN = 403
 }
